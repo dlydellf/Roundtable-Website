5f46643115917cbbf48db443aecb0bdc36c7b8ae
DForeman <dlydellf@hotmail.com>
2021-05-19 23:13:33 -0500

    main.js ~ Commented solution; possible redundancy in clearResult()

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,12 @@
 //dynamically load script tag into head tag based on option selected from dropdown
 function selectChallenge() {
   var file = document.getElementById("options").value;
+  // To PREVENT CREATING AN INFINITE AMOUNT OF <SCRIPTS>s, check if the <head>'s current <script> has this id; if so, remove it before appending a new one
   if (document.head.lastChild.id == "challenge-script") {
     document.head.lastChild.remove();
   }
   var newScriptTag = document.createElement("script");
-  newScriptTag.id = "challenge-script";
+  newScriptTag.id = "challenge-script"; // IMPORTANT!!! THIS CODE assigns an id to all newly created <script>s (see above comment)
   newScriptTag.src = `scripts/${file}.js`;
   document.head.appendChild(newScriptTag);
 }
@@ -17,5 +18,5 @@ function selectChallenge() {
 function clearResult() {
   document.getElementById("result-box").innerHTML = ""; // Clears the "results" <div>
   document.getElementById("options").value = "-- Select Date --"; // Resets the drop-down list
-  var newScriptTag = document.getElementById("challenge-script").remove(); // Removes the .js script from the DOM
+  var newScriptTag = document.getElementById("challenge-script").remove(); // Removes the .js script from the DOM (POSSIBLY REDUNDANT?? selectChallenge()'s if statement serves same purpose)
 }
